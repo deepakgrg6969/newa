65dd4314b1dfa53b81f2555d5b8a7aa724faf5e4
deepakgrg6969@gmail.com <your-email@example.com>
2026-06-09 16:35:48 +0900

    updating the image slider and media query

--- script.js
+++ script.js
@@ -45,3 +45,38 @@ const swiper = new Swiper('.swiper', {
    el: '.swiper-scrollbar',
  },
 });
+
+// image slider
+const container = document.querySelector(".rotate-slide-container");
+
+container.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("rotate-slide-img")) return;
+
+    const images = container.querySelectorAll(".rotate-slide-img");
+
+    // Click left image
+    if (e.target === images[0]) {
+        container.insertBefore(images[2], images[0]);
+    }
+
+    // Click right image
+    else if (e.target === images[2]) {
+        container.insertBefore(images[2], images[0]);
+    }
+
+    updateActive();
+});
+
+function updateActive() {
+    document.querySelectorAll(".rotate-slide-img").forEach(img =>
+        img.classList.remove("active", "poster-1", "poster-2", "poster-3")
+    );
+
+    const images = document.querySelectorAll(".rotate-slide-img");
+
+    images[0].classList.add("poster-1");
+    images[1].classList.add("poster-2", "active");
+    images[2].classList.add("poster-3");
+}
+
+updateActive();
